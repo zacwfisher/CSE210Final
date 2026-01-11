a16cafdb0989964daab660051687d6c644cd9c33
Zac <fis18010@byui.edu>
2026-01-11 00:58:32 -0500

    Prep 1&2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercent = Console.ReadLine();
+        int grade = int.Parse(gradePercent);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Your letter grade is {letter}.");
+        }
+        else
+        {
+            Console.WriteLine("You failed :( Better luck next time.");
+        }
     }
 }
